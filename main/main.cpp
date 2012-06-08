801ad6a2b12ae5f1d7633d570e21928e8bd6e2e8
Enzo Roberto Verlato <enzover@ig.com.br>
2012-06-08 08:32:08 -0300

    first commit

---
+++ main/main.cpp
@@ -0,0 +1,204 @@
+/*--------------------------------------------------------------------------
+    DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+    
+    File: main.cpp
+    Version: 1.0
+    Copyright: (C) 2012 by Enzo Roberto Verlato
+    Contact: enzover@ig.com.br
+    All rights reserved.
+    
+----------------------------------------------------------------------------
+    This file is part of the CommandLine Class.
+    
+    This file may be used under the terms of the GNU General Public
+    License version 2.0 as published by the Free Software Foundation
+    and appearing in the file LICENSE.GPL2 included in the packaging of
+    this file.
+    
+    This file is provided "AS IS" in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY OF ANY KIND, INCLUDING THE WARRANTIES OF DESIGN;
+    without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+    PARTICULAR PURPOSE. See the GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software Foundation,
+    Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+--------------------------------------------------------------------------*/
+
+#include <CommandLine.h>
+
+#include <iostream>
+#include <cstdlib>
+#include <stdexcept>
+
+using std::string;
+using std::cout;
+using std::endl;
+using std::runtime_error;
+
+int main() {
+    try {
+        util::CommandLine commandLine;
+        string option;
+        cout.precision( 9 );
+        
+        do {
+            option = commandLine.getParameter( commandLine.getCurrentPosition() + 1 );
+            
+            if ( commandLine.getCurrentParameter() == "getParametersNumber" ) {
+                
+                cout << commandLine.getParametersNumber() << endl;
+                
+            } else if( commandLine.getCurrentParameter() == "getCurrentPosition" ) {
+                
+                cout << commandLine.getCurrentPosition() << endl;
+                
+            } else if( commandLine.getCurrentParameter() == "getCommandLine" ) {
+                
+                cout << commandLine.getCommandLine() << endl;
+                
+            } else if( commandLine.getCurrentParameter() == "getApplicationName" ) {
+                
+                cout << commandLine.getApplicationName() << endl;
+                
+            } else if( commandLine.getCurrentParameter() == "getApplicationPath" ) {
+                
+                cout << commandLine.getApplicationPath() << endl;
+                
+            } else if( commandLine.getCurrentParameter() == "getApplicationFullPath" ) {
+                
+                cout << commandLine.getApplicationFullPath() << endl;
+                
+            } else if( commandLine.getCurrentParameter() == "getCurrentWorkingDirectory" ) {
+                
+                cout << commandLine.getCurrentWorkingDirectory() << endl;
+                
+            } else if( commandLine.getCurrentParameter() == "hasParameters" ) {
+                
+                commandLine.hasParameters() ? cout << "yes" << endl : cout << "no" << endl;
+                
+            } else if( commandLine.getCurrentParameter() == "hasParameter" ) {
+                
+                commandLine.hasParameter( commandLine.getParameterAsInteger( commandLine.getCurrentPosition() + 1 ) ) ? cout << "yes" << endl : cout << "no" << endl;
+                
+            } else if( commandLine.getCurrentParameter() == "getParameter" ) {
+                
+                cout << commandLine.getParameter( commandLine.getParameterAsInteger( commandLine.getCurrentPosition() + 1 ) ) << endl;
+                
+            } else if( commandLine.getCurrentParameter() == "getParameterAsInteger" ) {
+                
+                cout << commandLine.getParameterAsInteger( commandLine.getParameterAsInteger( commandLine.getCurrentPosition() + 1 ) ) << endl;
+                
+            } else if( commandLine.getCurrentParameter() == "getParameterAsFloat" ) {
+                
+                cout << commandLine.getParameterAsFloat( commandLine.getParameterAsInteger( commandLine.getCurrentPosition() + 1 ) ) << endl;
+                
+            } else if( commandLine.getCurrentParameter() == "gotoFirstParameter" ) {
+                
+                cout << "Tested Yet..." << endl;
+                
+            } else if( commandLine.getCurrentParameter() == "gotoNextParameter" ) {
+                
+                cout << "Tested Yet..." << endl;
+                
+            } else if( commandLine.getCurrentParameter() == "getAllParameters" ) {
+                
+                cout << commandLine.getAllParameters() << endl;
+                
+            } else if( commandLine.getCurrentParameter() == "getCurrentParameter" ) {
+                
+                cout << commandLine.getCurrentParameter() << endl;
+                
+            } else if( commandLine.getCurrentParameter() == "getCurrentParameterAsInteger" ) {
+                
+                cout << commandLine.getCurrentParameterAsInteger() << endl;
+                
+            } else if( commandLine.getCurrentParameter() == "getCurrentParameterAsFloat" ) {
+                
+                cout << commandLine.getCurrentParameterAsFloat() << endl;
+                
+            } else if( commandLine.getCurrentParameter() == "getFirstParameter" ) {
+                
+                cout << commandLine.getFirstParameter() << endl;
+                
+            } else if( commandLine.getCurrentParameter() == "getFirstParameterAsInteger" ) {
+                
+                cout << commandLine.getFirstParameterAsInteger() << endl;
+                
+            } else if( commandLine.getCurrentParameter() == "getFirstParameterAsFloat" ) {
+                
+                cout << commandLine.getFirstParameterAsFloat() << endl;
+                
+            } else if( commandLine.getCurrentParameter() == "getLastParameter" ) {
+                
+                cout << commandLine.getLastParameter() << endl;
+                
+            } else if( commandLine.getCurrentParameter() == "getLastParameterAsInteger" ) {
+                
+                cout << commandLine.getLastParameterAsInteger() << endl;
+                
+            } else if( commandLine.getCurrentParameter() == "getLastParameterAsFloat" ) {
+                
+                cout << commandLine.getLastParameterAsFloat() << endl;
+                
+            } else if( commandLine.getCurrentParameter() == "setOptionPrefix" ) {
+                
+                commandLine.setOptionPrefix( commandLine.getParameter( commandLine.getCurrentPosition() + 1 ) );
+                
+            } else if( commandLine.getCurrentParameter() == "setOptionPostfix" ) {
+                
+                commandLine.setOptionPostfix( commandLine.getParameter( commandLine.getCurrentPosition() + 1 ) );
+                
+            } else if( commandLine.getCurrentParameter() == "hasOption" ) {
+                
+                commandLine.hasOption( option ) ? cout << "yes" << endl : cout << "no" << endl;
+                
+            } else if( commandLine.getCurrentParameter() == "getOptionPrefix" ) {
+                
+                cout << commandLine.getOptionPrefix() << endl;
+                
+            } else if( commandLine.getCurrentParameter() == "getOptionPostfix" ) {
+                
+                cout << commandLine.getOptionPostfix() << endl;
+                
+            } else if( commandLine.getCurrentParameter() == "getOptionValue" ) {
+                
+                cout << commandLine.getOptionValue( option ) << endl;
+                
+            } else if( commandLine.getCurrentParameter() == "getOptionValueAsInteger" ) {
+                
+                cout << commandLine.getOptionValueAsInteger( option ) << endl;
+                
+            } else if( commandLine.getCurrentParameter() == "getOptionValueAsFloat" ) {
+                
+                cout << commandLine.getOptionValueAsFloat( option ) << endl;
+                
+            } else if( commandLine.getCurrentParameter() == "getOptionLongValue" ) {
+                
+                cout << commandLine.getOptionLongValue( option ) << endl;
+                
+            } else if( commandLine.getCurrentParameter() == "optionCaseSensitive" ) {
+                
+                commandLine.optionCaseSensitive();
+                
+            } else if( commandLine.getCurrentParameter() == "optionCaseInsensitive" ) {
+                
+                commandLine.optionCaseInsensitive();
+                
+            } else if( commandLine.getCurrentParameter() == "isOptionCaseSensitive" ) {
+                
+                commandLine.isOptionCaseSensitive() ? cout << "yes" << endl : cout << "no" << endl;
+            }
+        } while ( commandLine.gotoNextParameter() );
+        
+        return EXIT_SUCCESS;
+        
+    } catch ( runtime_error &error ) {
+        // Custom user messages...
+        
+        // Log messages...
+        cout << "Exception occurred at " << error.what() << endl;
+        
+        return EXIT_FAILURE;
+    }
+}
---
+++ src/CommandLine.cpp
@@ -0,0 +1,320 @@
+/*--------------------------------------------------------------------------
+    DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+    
+    File: CommandLine.cpp
+    Version: 1.0
+    Copyright: (C) 2012 by Enzo Roberto Verlato
+    Contact: enzover@ig.com.br
+    All rights reserved.
+    
+----------------------------------------------------------------------------
+    This file is part of the CommandLine Class.
+    
+    This file may be used under the terms of the GNU General Public
+    License version 2.0 as published by the Free Software Foundation
+    and appearing in the file LICENSE.GPL2 included in the packaging of
+    this file.
+    
+    This file is provided "AS IS" in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY OF ANY KIND, INCLUDING THE WARRANTIES OF DESIGN;
+    without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+    PARTICULAR PURPOSE. See the GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software Foundation,
+    Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+--------------------------------------------------------------------------*/
+
+#include <CommandLine.h>
+
+#include <algorithm>
+#include <stdexcept>
+
+#include <util.h>
+#include <compatibility.h>
+
+#define OPTION_NOT_FOUND -1
+
+namespace util {
+    
+    using std::runtime_error;
+    
+    CommandLine::CommandLine() {
+        try {
+            currentPosition = 0;
+            caseSensitiveMode = true;
+            
+            applicationFullPath = getExecutablePath();
+            parameters = getArguments();
+            optionParameters = parameters;
+            
+        } catch ( runtime_error &error ) {
+            // Clean up all previous efforts...
+            applicationFullPath.clear();
+            throw runtime_error( "FILE: " + string( __FILE__ ) + " FUNCTION: " + string( __PRETTY_FUNCTION__ ) + " -> " + error.what() );
+        }
+    }
+    
+    const string CommandLine::getCommandLine() const {
+        return applicationFullPath + " " + getAllParameters();
+    }
+    
+    const string CommandLine::getAllParameters() const {
+        string parameters;
+        for ( unsigned index = 0; index < this->parameters.size(); ++index ) {
+            parameters.append( this->parameters.at( index ) );
+            parameters.append( index + 1 == this->parameters.size() ? "" : " " );
+        }
+        return parameters;
+    }
+    
+    const string CommandLine::getApplicationName() const {
+        return applicationFullPath.substr( applicationFullPath.rfind( PATH_SEPARATOR ) + 1U );
+    }
+    
+    const string CommandLine::getApplicationPath() const {
+        return applicationFullPath.substr( 0, applicationFullPath.rfind( PATH_SEPARATOR ) );
+    }
+    
+    const string CommandLine::getApplicationFullPath() const {
+        return applicationFullPath;
+    }
+    
+    const string CommandLine::getCurrentWorkingDirectory() const {
+        try {
+            return getCurrentDirectory();
+        } catch ( runtime_error &error ) {
+            throw runtime_error( "FILE: " + string( __FILE__ ) + " FUNCTION: " + string( __PRETTY_FUNCTION__ ) + " -> " + error.what() );
+        }
+    }
+    
+    const bool CommandLine::hasParameters() const {
+        return parameters.empty() ? false : true;
+    }
+    
+    const bool CommandLine::hasParameter( const unsigned &position ) const {
+        return position <= parameters.size() && position > 0 ? true : false;
+    }
+    
+    const int CommandLine::getParametersNumber() const {
+        return parameters.size();
+    }
+    
+    const string CommandLine::getParameter( const unsigned &position ) const {
+        return position <= parameters.size() && position > 0 ? parameters.at( position - 1 ) : "";
+    }
+    
+    const int CommandLine::getParameterAsInteger( const unsigned &position ) const {
+        return atoi( getParameter( position ).c_str() );
+    }
+    
+    const double CommandLine::getParameterAsFloat( const unsigned &position ) const {
+        return atof( getParameter( position ).c_str() );
+    }
+    
+    void CommandLine::gotoFirstParameter() {
+        currentPosition = 0;
+    }
+    
+    const bool CommandLine::gotoNextParameter() {
+        if ( currentPosition + 1 < parameters.size() ) {
+            ++currentPosition;
+            return true;
+        }
+        return false;
+    }
+    
+    const int CommandLine::getCurrentPosition() const {
+        return parameters.size() ? currentPosition + 1 : 0;
+    }
+    
+    const string CommandLine::getCurrentParameter() const {
+        return getParameter( currentPosition + 1 );
+    }
+    
+    const int CommandLine::getCurrentParameterAsInteger() const {
+        return getParameterAsInteger( currentPosition + 1 );
+    }
+    
+    const double CommandLine::getCurrentParameterAsFloat() const {
+        return getParameterAsFloat( currentPosition + 1 );
+    }
+    
+    const string CommandLine::getFirstParameter() const {
+        return getParameter( 1 );
+    }
+    
+    const int CommandLine::getFirstParameterAsInteger() const {
+        return getParameterAsInteger( 1 );
+    }
+    
+    const double CommandLine::getFirstParameterAsFloat() const {
+        return getParameterAsFloat( 1 );
+    }
+    
+    const string CommandLine::getLastParameter() const {
+        return getParameter( parameters.size() );
+    }
+    
+    const int CommandLine::getLastParameterAsInteger() const {
+        return getParameterAsInteger( parameters.size() );
+    }
+    
+    const double CommandLine::getLastParameterAsFloat() const {
+        return getParameterAsFloat( parameters.size() );
+    }
+    
+    void CommandLine::setOptionPrefix( const string &optionPrefix ) {
+        this->postfixed = false;
+        this->optionPrefix = optionPrefix;
+        optionParameters = parameters;
+    }
+    
+    void CommandLine::setOptionPostfix( const string &optionPostfix ) {
+        if ( !optionPostfix.empty() ) {
+            this->postfixed = true;
+            this->optionPrefix = optionPostfix;
+            convertOptionPostfixToPrefix();
+        }
+    }
+    
+    void CommandLine::convertOptionPostfixToPrefix() {
+        vector<string> parameters = split( prefixAndPostfixOptionPostfixWhithWhitespace( getAllParameters() ), " " );
+        parameters = removeNullElement( parameters );
+        parameters = removeOptionPostfixDuplicityBetweenOptionAndValue( parameters );
+        
+        for ( unsigned position = 0; position < parameters.size(); ++position ) {
+            if ( position > 0 && parameters.at( position ) == optionPrefix ) {
+                if ( parameters.at( position - 1 ).find( optionPrefix ) == string::npos ) {
+                    parameters.at( position - 1 ) = optionPrefix + parameters.at( position - 1 );
+                }
+                
+                parameters.erase( parameters.begin() + position );
+                --position;
+            }
+        }
+        optionParameters = parameters;
+    }
+    
+    const string CommandLine::prefixAndPostfixOptionPostfixWhithWhitespace( string parameters ) const {
+        size_t found = parameters.find( optionPrefix, 0 );
+        while ( found != string::npos ) {
+            parameters.replace( parameters.find( optionPrefix, found ), optionPrefix.size(), " " + optionPrefix + " " );
+            found += 2 + optionPrefix.size();
+            found = parameters.find( optionPrefix, found );
+        }
+        return parameters;
+    }
+    
+    const vector<string> CommandLine::removeNullElement( vector<string> parameters ) const {
+        for ( unsigned position = 0; position < parameters.size(); ++position ) {
+            if ( parameters.at( position ).empty() ) {
+                parameters.erase( parameters.begin() + position );
+                --position;
+            }
+        }
+        return parameters;
+    }
+    
+    const vector<string> CommandLine::removeOptionPostfixDuplicityBetweenOptionAndValue( vector<string> parameters ) const {
+        for ( unsigned position = 0; position < parameters.size(); ++position ) {
+            if ( position + 1 < parameters.size() && parameters.at( position ) == optionPrefix && parameters.at( position + 1 ) == optionPrefix ) {
+                parameters.erase( parameters.begin() + position );
+                --position;
+            }
+        }
+        return parameters;
+    }
+    
+    const string CommandLine::getOptionPrefix() const {
+        return postfixed ? "" : optionPrefix;
+    }
+    
+    const string CommandLine::getOptionPostfix() const {
+        return postfixed ? optionPrefix : "";
+    }
+    
+    const bool CommandLine::hasOption( const string &option ) const {
+        return findOptionPosition( option ) != OPTION_NOT_FOUND ? true : false;
+    }
+    
+    const int CommandLine::getOptionValueAsInteger( const string &option ) const {
+        return atoi( getOptionValue( option ).c_str() );
+    }
+    
+    const double CommandLine::getOptionValueAsFloat( const string &option ) const {
+        return atof( getOptionValue( option ).c_str() );
+    }
+    
+    const string CommandLine::getOptionValue( const string &option ) const {
+        int index = findOptionPosition( option );
+        if ( index != OPTION_NOT_FOUND && unsigned( ++index ) < optionParameters.size() ) {
+            if ( optionPrefix.empty() ) {
+                return optionParameters.at( index );
+            }
+            
+            size_t found = optionParameters.at( index ).find( optionPrefix );
+            
+            if ( found == string::npos || found > 0 ) {
+                return optionParameters.at( index );
+            }
+        }
+        return "";
+    }
+    
+    const string CommandLine::getOptionLongValue( const string &option ) const {
+        int index = findOptionPosition( option );
+        string parameters;
+        if ( index != OPTION_NOT_FOUND ) {
+            size_t found;
+            for ( ++index; unsigned( index ) < this->optionParameters.size(); ++index ) {
+                found = optionPrefix.empty() || this->optionParameters.at( index ) == optionPrefix ? string::npos : this->optionParameters.at( index ).find( optionPrefix );
+                
+                if ( found == string::npos || found > 0 ) {
+                    parameters += this->optionParameters.at( index );
+                } else {
+                    break;
+                }
+                
+                if ( unsigned( index + 1 ) < this->optionParameters.size() ) {
+                    parameters += " ";
+                }
+            }
+        }
+        return trim( parameters );
+    }
+    
+    const int CommandLine::findOptionPosition( string option ) const {
+        if ( !option.empty() ) {
+            if ( !caseSensitiveMode ) {
+                transform( option.begin(), option.end(), option.begin(), ::tolower );
+            }
+            
+            string parameter;
+            for ( unsigned index = 0; index < optionParameters.size(); ++index ) {
+                parameter = optionParameters.at( index );
+                
+                if ( !caseSensitiveMode ) {
+                    transform( parameter.begin(), parameter.end(), parameter.begin(), ::tolower );
+                }
+                
+                if ( optionPrefix + option == parameter ) {
+                    return index;
+                }
+            }
+        }
+        return OPTION_NOT_FOUND;
+    }
+    
+    void CommandLine::optionCaseSensitive() {
+        caseSensitiveMode = true;
+    }
+    
+    void CommandLine::optionCaseInsensitive() {
+        caseSensitiveMode = false;
+    }
+    
+    const bool CommandLine::isOptionCaseSensitive() const {
+        return caseSensitiveMode;
+    }
+}
