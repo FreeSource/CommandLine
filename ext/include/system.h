1419e2cc8924a9df20155b0d229e2fb4f64934a9
FreeSource <enzover@ig.com.br>
2013-01-13 22:06:30 -0200

    minor changes.

--- ext/include/system.h
+++ ext/include/system.h
@@ -24,8 +24,8 @@
     along with this program; if not, write to the Free Software Foundation,
     Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 --------------------------------------------------------------------------*/
-#ifndef ENVIRONMENT_H
-#define ENVIRONMENT_H
+#ifndef SYSTEM_H
+#define SYSTEM_H
 
 #include <string>
 #include <vector>
